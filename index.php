5dbbc621e15bd8b2bdd7356846e9261401d64972
Arnika Jain <arnikajain94@gmail.com>
2019-04-15 16:24:09 +0530

    Add service names and images

--- index.php
+++ index.php
@@ -7,9 +7,33 @@
   <body>
     <div class = "page-container">
       <div class = "left-container">
-        <div class = "service-container">
+        <div class = "service-heading-container">
           <img src = "images/darkBackground" class = "dark-background">
           <div class = "service-text">OUR SERVICES</div>
+        </div>
+        <div class = "service-image-container">
+          <img src = "images/unknownImage.jpg" class = "service-image" alt = "service">
+          <img src = "images/unknownImage.jpg" class = "service-image" alt = "service">
+        </div>
+        <div class = "service-name-container">
+          <div class = "service-name">Service 1</div>
+          <div class = "service-name">Service 2</div>
+        </div>
+        <div class = "service-image-container">
+          <img src = "images/unknownImage.jpg" class = "service-image" alt = "service">
+          <img src = "images/unknownImage.jpg" class = "service-image" alt = "service">
+        </div>
+        <div class = "service-name-container">
+          <div class = "service-name">Service 1</div>
+          <div class = "service-name">Service 2</div>
+        </div>
+        <div class = "service-image-container">
+          <img src = "images/unknownImage.jpg" class = "service-image" alt = "service">
+          <img src = "images/unknownImage.jpg" class = "service-image" alt = "service">
+        </div>
+        <div class = "service-name-container">
+          <div class = "service-name">Service 1</div>
+          <div class = "service-name">Service 2</div>
         </div>
       </div>
       <div class = "mid-container"></div>
