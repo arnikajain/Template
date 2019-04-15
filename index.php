ae8718d28018c31625552cf93baed78f89f152d5
Arnika Jain <arnikajain94@gmail.com>
2019-04-15 14:39:27 +0530

    Add the background to the template

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-
+<!DOCTYPE html>
+  <head>
+    <title>Template</title>
+    <link rel="stylesheet" href = "style.css">
+  </head>
+  <body>
+    <div class = "page-container">
+      <div class = "left-container"></div>
+      <div class = "mid-container"></div>
+      <div class = "right-container"></div>
+    </div>
+  </body>
+</html>
