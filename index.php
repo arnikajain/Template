1cc2ccfbd109dd2a2570b210816b1b0e363df976
Arnika Jain <arnikajain94@gmail.com>
2019-04-15 15:27:01 +0530

    Add service heading to the template

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
   <head>
     <title>Template</title>
+    <meta charset="utf-8">
     <link rel="stylesheet" href = "style.css">
   </head>
   <body>
     <div class = "page-container">
-      <div class = "left-container"></div>
+      <div class = "left-container">
+        <div class = "service-container">
+          <img src = "images/darkBackground" class = "dark-background">
+          <div class = "service-text">OUR SERVICES</div>
+        </div>
+      </div>
       <div class = "mid-container"></div>
       <div class = "right-container"></div>
     </div>
